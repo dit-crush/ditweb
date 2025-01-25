f365a424fb6e1708b12d5c699870a76ea06ff604
ditcrush <289782449@qq.com>
2025-01-25 20:10:51 +0800

    first commit

---
+++ src/main/java/dit/crush/ditcrush/DitCrushApplication.java
@@ -0,0 +1,13 @@
+package dit.crush.ditcrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitCrushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DitCrushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dit/crush/ditcrush/DitCrushApplicationTests.java
@@ -0,0 +1,13 @@
+package dit.crush.ditcrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitCrushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
